f48d553209cc62be169fcdb58f6d2009a23711c5
javadbat <javadbat@gmail.com>
2025-01-30 01:21:02 +0330

    build from inside dir

---
+++ build-config.ts
@@ -0,0 +1,27 @@
+import type { ReactComponentBuildConfig, WebComponentBuildConfig } from "../../tasks/build/builders/src/types.ts";
+
+export const webComponentList: WebComponentBuildConfig[] = [
+  {
+    name: "jb-date-input",
+    path: "./lib/jb-date-input.ts",
+    outputPath: "./dist/jb-date-input.js",
+    umdName: "JBDateInput",
+    external: ["date-fns", "date-fns-jalali", "jb-calendar","jb-input","jb-popover","jb-validation"],
+    //because date-fns dont have any umd module export i have to do this so it doesn't exclude in umd build
+    umdIncludes: ["date-fns", "date-fns-jalali", "jb-calendar","jb-input","jb-popover","jb-validation"],
+  },
+];
+export const reactComponentList: ReactComponentBuildConfig[] = [
+  {
+    name: "jb-date-input-react",
+    path: "./react/lib/JBDateInput.tsx",
+    outputPath: "./react/dist/JBDateInput.js",
+    external: ["react", "prop-types", "jb-date-input", "jb-validation"],
+    globals: {
+      react: "React",
+      "prop-types": "PropTypes",
+      "jb-date-input": "JBDateInput",
+      "jb-validation":"JBValidation",
+    },
+  },
+];
